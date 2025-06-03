cd0b8835d193930857172517c49263e981211102
Kaiyan2 <kherrera0129@mymail.lausd.net>
2025-06-03 09:13:18 -0700

    testing

--- src/main/java/com/kaiyan/SquareFinch.java
+++ src/main/java/com/kaiyan/SquareFinch.java
@@ -6,12 +6,13 @@ public class SquareFinch {
     public static void main(String[] args) {
         Finch alston = new Finch();
         alston.setMotors(100,100);
+        alston.playNote(55, 2);
 
         for (int i = 0; i<4; i++){
         alston.setMove("F",10,100);
         alston.setTurn("R",90,100);
-       //alston.setBeak(100, 0, 0);
-       //alston.getLight("L");
+       alston.setBeak(100, 0, 0);
+       alston.getLight("L");
         }
         alston.setTail(2,0,0,100);
         alston.setMove("F", 10, 100);
@@ -19,4 +20,5 @@ public class SquareFinch {
         alston.stop();
         alston.disconnect();
     }
+
 }
