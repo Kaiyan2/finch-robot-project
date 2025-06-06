190b513c6d9af6761f1803789595291f6d3d60f2
Kaiyan2 <kherrera0129@mymail.lausd.net>
2025-06-06 13:19:02 -0700

    Complete

--- src/main/java/com/kaiyan/SquareFinch.java
+++ src/main/java/com/kaiyan/SquareFinch.java
@@ -5,15 +5,20 @@ import com.birdbrain.Finch;
 public class SquareFinch {
     public static void main(String[] args) {
         Finch alston = new Finch();
+        //sets the robot motors to full speed
         alston.setMotors(100,100);
+        //plays a note and gives the number of beats
         alston.playNote(55, 2);
 
+//Turns on the beak lights and moves forward and right
         for (int i = 0; i<4; i++){
         alston.setMove("F",10,100);
         alston.setTurn("R",90,100);
        alston.setBeak(100, 0, 0);
        alston.getLight("L");
         }
+
+        //sets the tail to a color and plays a sequence of movements
         alston.setTail(2,0,0,100);
         alston.playNote(69, 2);
         alston.setMove("F", 10, 100);
